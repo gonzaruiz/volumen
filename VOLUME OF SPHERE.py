a633be9b66886c68073ad6e326859bf07b09bd49
gonzalo ruiz <38412697+gonzaruiz@users.noreply.github.com>
2018-04-17 13:47:40 -0500

    Add files via upload

---
+++ VOLUME OF SPHERE.py
@@ -0,0 +1,28 @@
+
+# coding: utf-8
+
+# In[27]:
+
+
+# VOLUME OF SPHERE
+name1 = "v"
+d = 12
+r = (d/2)
+pi = 3
+x = 4/3
+
+
+# In[28]:
+
+
+def volumen_calculator(name1, d, r, pi, x):
+    volumen = ( x * pi * (r*3))
+    print ("volumen is: ")
+    print (volumen)
+
+
+# In[29]:
+
+
+result = volumen_calculator(name1, d, r, pi, x)
+
